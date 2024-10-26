aaa5b25eea0e0099d4e516d8c9a0aeae30ce4d49
yonas_fikadie <yonasfikadie8989@gmail.com>
2024-10-26 10:41:44 +0200

    create a model , factory  , seeder and migration for employer

--- config/queue.php
+++ config/queue.php
@@ -36,7 +36,7 @@ return [
 
         'database' => [
             'driver' => 'database',
-            'table' => 'jobs',
+            'table' => 'queued_jobs',
             'queue' => 'default',
             'retry_after' => 90,
             'after_commit' => false,
@@ -86,7 +86,7 @@ return [
 
     'batching' => [
         'database' => env('DB_CONNECTION', 'mysql'),
-        'table' => 'job_batches',
+        'table' => 'queued_job_batches',
     ],
 
     /*
@@ -103,7 +103,7 @@ return [
     'failed' => [
         'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
         'database' => env('DB_CONNECTION', 'mysql'),
-        'table' => 'failed_jobs',
+        'table' => 'queued_failed_jobs',
     ],
 
 ];
--- database/migrations/2024_10_26_082234_create_employers_table.php
+++ database/migrations/2024_10_26_082234_create_employers_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,14 +12,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('queued_failed_jobs', function (Blueprint $table) {
+        Schema::create('employers', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name');
+            $table->string('logo');
+            $table->foreignIdFor(User::class);
+            $table->timestamps();
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('queued_failed_jobs');
+        Schema::dropIfExists('employers');
     }
 };
