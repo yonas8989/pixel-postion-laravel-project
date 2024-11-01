6078f5ba032710e36b0018f5b12168294ebfa588
yonas_fikadie <yonasfikadie8989@gmail.com>
2024-11-01 10:37:52 +0200

    make the email delivery async or at the real time by through it to queue so that bring fast response

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -45,7 +45,7 @@ class JobController extends Controller
             }
         }
 
-        Mail::to($job->employer->user)->send(new JobPosted($job));
+        Mail::to($job->employer->user)->queue(new JobPosted($job));
 
         // after that redirect 
 
